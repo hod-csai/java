dd8254654cd79faa01ad173dfdb54b7b4fc09218
externref <shiva02939@gmail.com>
2026-05-18 02:20:59 +0530

    prog8

--- src/main/java/com/example/FormServlet.java
+++ src/main/java/com/example/FormServlet.java
@@ -1,44 +1,68 @@
 package com.example;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+@WebServlet("/FormServlet")
 public class FormServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        String name = request.getParameter("username");
-        String email = request.getParameter("email");
-        String age = request.getParameter("age");
-        String gender = request.getParameter("gender");
-        String country = request.getParameter("country");
-        String subscribe = request.getParameter("subscribe");
-        String comments = request.getParameter("comments");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
 
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h3>Form submission received</h3>");
-        out.println("<table border='1' cellpadding='6'>");
-        out.println(tr("Name", name));
-        out.println(tr("Email", email));
-        out.println(tr("Age", age));
-        out.println(tr("Gender", gender));
-        out.println(tr("Country", country));
-        out.println(tr("Subscribe", subscribe == null ? "no" : escapeHtml(subscribe)));
-        out.println(tr("Comments", comments));
-        out.println("</table>");
-        out.println("</body></html>");
+        String username = safe(req.getParameter("username"));
+        String password = req.getParameter("password"); // do not display raw
+        String email = safe(req.getParameter("email"));
+        String age = safe(req.getParameter("age"));
+        String gender = safe(req.getParameter("gender"));
+        String country = safe(req.getParameter("country"));
+        String subscribe = req.getParameter("subscribe") != null ? "yes" : "no";
+        String comments = safe(req.getParameter("comments"));
+
+        // store a simple representation in application scope
+        @SuppressWarnings("unchecked")
+        List<String> users = (List<String>) getServletContext().getAttribute("registeredUsers");
+        if (users == null) {
+            users = new CopyOnWriteArrayList<>();
+            getServletContext().setAttribute("registeredUsers", users);
+        }
+
+        String rec = "User: " + username + ", Email: " + email + ", Age: " + age + ", Gender: " + gender + ", Country: " + country + ", Subscribed: " + subscribe;
+        users.add(rec);
+
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<html><body>");
+            out.println("<h2>Registration successful</h2>");
+            out.println("<p>Thank you, " + username + "</p>");
+            out.println("<ul>");
+            out.println("<li>Email: " + email + "</li>");
+            out.println("<li>Age: " + age + "</li>");
+            out.println("<li>Gender: " + gender + "</li>");
+            out.println("<li>Country: " + country + "</li>");
+            out.println("<li>Subscribed: " + subscribe + "</li>");
+            out.println("</ul>");
+
+            out.println("<h3>Registered users</h3>");
+            out.println("<ol>");
+            for (String u : users) {
+                out.println("<li>" + u + "</li>");
+            }
+            out.println("</ol>");
+
+            out.println("<p><a href=\"index.jsp\">Back</a></p>");
+            out.println("</body></html>");
+        }
     }
 
-    private String tr(String label, String value) {
-        return "<tr><td><strong>" + escapeHtml(label) + "</strong></td><td>" + (value == null ? "" : escapeHtml(value)) + "</td></tr>";
-    }
-
-    private String escapeHtml(String s) {
-        return s.replace("&","&amp;")
-                .replace("<","&lt;")
-                .replace(">","&gt;")
-                .replace("\"","&quot;");
+    private String safe(String s) {
+        if (s == null) return "";
+        return s.replace("&","&amp;").replace("<","&lt;").replace(">","&gt;").replace("\"","&quot;");
     }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" %>
+<html>
+<body>
+    <jsp:include page="header.jsp" />
+    <p>This is the main page content.</p>
+    <jsp:include page="form.jsp" />
+</body>
+</html>
